19864d3f677ac79df9f795a8e73d8fb731730921
Rasel <you@example.com>
2024-10-20 22:59:02 +0600

    function

---
+++ function-parameter.js
@@ -0,0 +1,11 @@
+function squre(number){
+    const sum=number*number;
+    console.log(sum);
+}
+squre(40);
+
+function addTotal(num1, num2){
+    const total=num1+num2;
+    console.log(total);
+}
+addTotal(233,2);
