b3c7d9ea4fb541f6050d1ea0d0fba476e5f5aa08
Rasel <you@example.com>
2024-11-10 23:46:32 +0600

    conditional return

---
+++ condition-return.js
@@ -0,0 +1,17 @@
+function isEven(number){
+    if(number%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(15));
+
+function isOdd(num){
+    if(num%2!=0){
+        return true;
+    }
+    return false;
+}
+console.log(isOdd(5));
---
+++ recap.js
@@ -0,0 +1,9 @@
+function doMath(num1, num2){
+    const add=num1+num2;
+    const dif=num1-num2;
+    const multiple=add*dif;
+    const div=multiple/2;
+    return div;
+}
+const div=doMath(100, 15);
+console.log(div);
