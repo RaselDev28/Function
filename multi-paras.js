0e18c7ebde5749267165c168ee60eadefea2ac36
Rasel <you@example.com>
2024-11-11 23:27:51 +0600

    fd

---
+++ multi-paras.js
@@ -0,0 +1,12 @@
+function oddOrEven(str){
+    if(str%2==0){
+        console.log("Even");
+        return true;
+    }
+    else{
+        console.log("Odd");
+        return false;
+    }
+}
+// oddOrEven("Dhaka");
+console.log(oddOrEven("faka"));
